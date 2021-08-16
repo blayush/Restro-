9f3d1363bf1272633de966120d6ce130aa55a4fb
Ayush Mishra <ayushmishra42@gmail.com>
2021-08-16 11:57:42 +0530

    adapter and model for order activity made fully

--- app/src/main/java/com/example/boomshiva/OrderPageActivity.java
+++ app/src/main/java/com/example/boomshiva/OrderPageActivity.java
@@ -20,14 +20,14 @@ public class OrderPageActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         ArrayList<OrderPageModel>list=new ArrayList<>();
-        list.add(new OrderPageModel((R.drawable.paneerroll),"Paneer Roll","79","301"));
-        list.add(new OrderPageModel((R.drawable.malaikofta),"Malai Kofta","189","303"));
-        list.add(new OrderPageModel((R.drawable.vegroll),"Veg Roll","39","305"));
-        list.add(new OrderPageModel((R.drawable.shahipaneer),"Shahi Paneer","159","302"));
-        list.add(new OrderPageModel((R.drawable.paneerroll),"Paneer Roll","79","301"));
-        list.add(new OrderPageModel((R.drawable.malaikofta),"Malai Kofta","189","303"));
-        list.add(new OrderPageModel((R.drawable.vegroll),"Veg Roll","39","305"));
-        list.add(new OrderPageModel((R.drawable.shahipaneer),"Shahi Paneer","159","302"));
+        list.add(new OrderPageModel((R.drawable.paneerroll),"Paneer Roll","79 Rs.","301"));
+        list.add(new OrderPageModel((R.drawable.malaikofta),"Malai Kofta","189 Rs.","303"));
+        list.add(new OrderPageModel((R.drawable.vegroll),"Veg Roll","39 Rs.","305"));
+        list.add(new OrderPageModel((R.drawable.shahipaneer),"Shahi Paneer","159 Rs.","302"));
+        list.add(new OrderPageModel((R.drawable.paneerroll),"Paneer Roll","79 Rs.","301"));
+        list.add(new OrderPageModel((R.drawable.malaikofta),"Malai Kofta","189 Rs.","303"));
+        list.add(new OrderPageModel((R.drawable.vegroll),"Veg Roll","39 Rs.","305"));
+        list.add(new OrderPageModel((R.drawable.shahipaneer),"Shahi Paneer","159 Rs.","302"));
         OrderPageAdapter adapter=new OrderPageAdapter(list,this);
         binding.orderRecyclerView.setAdapter(adapter);
         LinearLayoutManager linearLayoutManager=new LinearLayoutManager(this);
