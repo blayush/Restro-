0e4680e4ea5a2f02d035cac2d35dd5fc44346724
Ayush Mishra <ayushmishra42@gmail.com>
2021-08-28 22:20:48 +0530

    order detail activity + image upload

--- app/src/main/java/com/example/boomshiva/SignupActivity.java
+++ app/src/main/java/com/example/boomshiva/SignupActivity.java
@@ -3,17 +3,36 @@ package com.example.boomshiva;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.TextView;
+
+import com.github.dhaval2404.imagepicker.ImagePicker;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class SignupActivity extends AppCompatActivity {
-
+    TextView uploadTextButton;
+    CircleImageView uploadImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        uploadTextButton =findViewById(R.id.uploadTextView);
+        uploadImage=findViewById(R.id.profile_image);
         setContentView(R.layout.activity_signup);
         getWindow().setFlags(
                 WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
                 WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        uploadTextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImagePicker.with(SignupActivity.this)
+                        .crop()	    			//Crop image(Optional), Check Customization for more option
+                        .compress(1024)			//Final image size will be less than 1 MB(Optional)
+                        .maxResultSize(1080, 1080)	//Final image resolution will be less than 1080 x 1080(Optional)
+                        .start();
+            }
+        });
 
     }
 }
