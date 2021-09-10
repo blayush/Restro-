cc7df37b0c38d0873b9cc069481d6cbd5ae240a0
Ayush Mishra <ayushmishra42@gmail.com>
2021-09-10 08:53:12 +0530

    firebase auth login

--- app/src/main/java/com/example/boomshiva/LoginActivity.java
+++ app/src/main/java/com/example/boomshiva/LoginActivity.java
@@ -1,15 +1,21 @@
 package com.example.boomshiva;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 import com.example.boomshiva.databinding.ActivityLoginBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
@@ -32,7 +38,19 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 progressDialog.show();
-                mAuth.signInWithEmailAndPassword(binding.editTextemail.getText().toString(),binding.editTextPassword.getText().toString());
+                mAuth.signInWithEmailAndPassword(binding.editTextemail.getText().toString(),binding.editTextPassword.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressDialog.dismiss();
+                        if(task.isSuccessful()){
+                            Intent intent=new Intent(LoginActivity.this,FoodActivity.class);
+                            startActivity(intent);
+                        }
+                        else{
+                            Toast.makeText(LoginActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
 
 
             }
