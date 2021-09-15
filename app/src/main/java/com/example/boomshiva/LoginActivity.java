4e31f850bc6de32618201caddfe8d2bef7ba501f
Ayush Mishra <ayushmishra42@gmail.com>
2021-09-15 22:47:06 +0530

    firebase auth login

--- app/src/main/java/com/example/boomshiva/LoginActivity.java
+++ app/src/main/java/com/example/boomshiva/LoginActivity.java
@@ -38,12 +38,12 @@ public class LoginActivity extends AppCompatActivity {
         progressDialog.setTitle("Log-in");
         progressDialog.setMessage("Logging you in..");
         mAuth=FirebaseAuth.getInstance();
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
-                .requestEmail()
-                .build();
-
-        googleSignInClient = GoogleSignIn.getClient(this, gso);
+//        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+//                .requestIdToken(getString(R.string.default_web_client_id))
+//                .requestEmail()
+//                .build();
+//
+//        googleSignInClient = GoogleSignIn.getClient(this, gso);
         binding.loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
