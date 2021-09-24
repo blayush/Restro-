c168a1f5bd7086ed7825a3e0395191f2ab249b9a
Ayush Mishra <ayushmishra42@gmail.com>
2021-09-24 23:09:51 +0530

    food detail activity + sqlite

--- app/src/main/java/com/example/boomshiva/OrderDetailsActivity.java
+++ app/src/main/java/com/example/boomshiva/OrderDetailsActivity.java
@@ -13,7 +13,15 @@ public class OrderDetailsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         activityOrderDetailsBinding=ActivityOrderDetailsBinding.inflate(getLayoutInflater());
         setContentView(activityOrderDetailsBinding.getRoot());
+        int price=Integer.parseInt(getIntent().getStringExtra("price"));
+        int image=getIntent().getIntExtra("image",0);
+        String description=getIntent().getStringExtra("desc");
+        String name=getIntent().getStringExtra("name");
 
+        activityOrderDetailsBinding.foodImage.setImageResource(image);
+        activityOrderDetailsBinding.foodName.setText(name);
+        activityOrderDetailsBinding.price.setText("Rs. "+(String.format("%d",price)));
+        activityOrderDetailsBinding.descTextView.setText(description);
 
 
     }
